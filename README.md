7a5cfcb9cfcd153e231796ff6e7d43966a08df7d
theMitch36 <devmakos36@gmail.com>
2018-08-18 13:35:44 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# crypto-network-basic
+
+basic crypto network containing 3 nodes, each running independent on PORT specified in file name.
+network endpoints exposed via flask.
+contains json needed to connect nodes to the network, and json needed to create & send  a transaction
+
---
+++ moneymitchcoin5003.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  7 14:31:31 2018
+
+@author: mitchellmakos
+"""
+
+import datetime 
+import hashlib
+import json
+from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+
+# Part 1 - build the block chain
+
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.create_block(proof = 1, previous_hash = '0')
+        self.nodes = set()
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash,
+                 'transactions': self.transactions}
+        self.transactions = []
+        self.chain.append(block)
+        return block 
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+        
+    def hash(slef, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index +=1
+        return True
+    
+    def add_transaction(self, sender, reciever, amount):
+        self.transactions.append({'sender': sender, 
+                                  'reciever': reciever, 
+                                  'amount': amount})
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
+    
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+        
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+    
+# Create a we app
+app = Flask(__name__)
+
+#Create a UUID for the address on our local port
+node_address = str(uuid4()).replace('-', '')
+    
+# Part 2 - mining the blockchain
+blockchain = Blockchain()
+    
+# Mine a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender = node_address, reciever = 'Mitch', amount = 1000)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Success',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash'],
+                'transactions': block['transactions']}
+    return jsonify(response), 200
+
+# Get the whole chain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good, the blockchain is up and good to go!'}
+    else: 
+        response = {'message': 'Ya fucked er bud!'}
+    return jsonify(response), 200
+
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all (key in json for key in transaction_keys):
+        return 'Yo dawg, you done fucked up. check yo shit and try again'
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'WHATS UPPP SONNNNN... We about to add yo shit to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return 'Yo Dawg, wheres the Node at?'
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'My Dude... i gots good news... ALL R SHITS IS CONNECTED MOTHA FUCKA. The MoneyMitch Blockchain is all yall MFs',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'Yo Dawg we had a problem. Someshit got fucked up, but no fret ninja! Our shit is fuckn solid, just did some computer shit and fixed errthang',
+                    'new_chain': blockchain.chain}
+    else: 
+        response = {'message': 'We all good in the hood ninja! All the shit checks out',
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
+        
+
+#Run the app
+app.run(host = '0.0.0.0', port = 5003)
+    
+    
+    
+    
+            
